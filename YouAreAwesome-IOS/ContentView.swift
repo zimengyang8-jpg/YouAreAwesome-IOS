529af0a34ac9a0260c9f239256475a1cc50e7472
Zimeng Yang <zimengyang8@gmail.com>
2026-02-01 13:41:19 -0500

    Initial Commit

---
+++ YouAreAwesome-IOS/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  YouAreAwesome-IOS
+//
+//  Created by ZM on 2/1/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ YouAreAwesome-IOS/YouAreAwesome_IOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  YouAreAwesome_IOSApp.swift
+//  YouAreAwesome-IOS
+//
+//  Created by ZM on 2/1/26.
+//
+
+import SwiftUI
+
+@main
+struct YouAreAwesome_IOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
